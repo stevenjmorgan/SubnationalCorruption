3747ee65ff4d5e23e638f58949129a2b5e1458b1
Steven Morgan <sum410@psu.edu>
2018-10-03 16:55:49 -0400

    Load packages

--- Scrapers/people_daily_scrape.py
+++ Scrapers/people_daily_scrape.py
@@ -1,1 +1,18 @@
+# -*- coding: utf-8 -*-
 
+import os
+import re
+import sys
+import random
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# Initiate web driver
+path_to_chromedriver = 'C:/Users/sum410/Desktop/chromedriver'
+browser = webdriver.Chrome(executable_path = path_to_chromedriver)
+
+url = 'http://search.people.com.cn/language/english/getResult.jsp'
