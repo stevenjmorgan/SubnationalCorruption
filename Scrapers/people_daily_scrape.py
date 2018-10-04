1d1fda6527a332eac8fcb7f9648cfe1e57641ae1
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2018-10-03 20:50:53 -0400

    Test on first 2 search pages

--- Scrapers/people_daily_scrape.py
+++ Scrapers/people_daily_scrape.py
@@ -34,39 +34,44 @@ search_bar = browser.find_element_by_css_selector("input[id='keyword']")
 search_bar = browser.find_element_by_id('keyword')
 search_bar.send_keys('corrupt' + '\n')
 
+seed = 24519
+random.seed(seed)
 counter = 0
 title = ''
 content_par = ''
 
 # Iterate through links (nested for loop)
-for i in range(0,2): # number of search result pages 152
-    
-    
-    
+for i in range(0,3): # number of search result pages 152
+
+    if i % 5 == 1:
+        time.sleep(5)
+
     elems = browser.find_elements_by_xpath("//a[@href]")
     elems = [x for x in elems if re.search('http://english.people.com.cn/n3/', str(x.get_attribute("href")))]
     print(len(elems))
     elems = elems[::2] # this should probably be set
+    elems = [a.get_attribute('href') for a in elems]
     #print([x.get_attribute("href") for x in elems])
-    
+
     ### Access each link and scrape contents
-    for link in elems:
-        
+    for link in elems[8:]:
+
         counter += 1
-        browser.get(link.get_attribute('href')) # Go to document
-        
+        #browser.get(link.get_attribute('href')) # Go to document
+        browser.get(link)
+
         try:
             title = browser.find_element_by_tag_name('h1').text.strip()
         except:
             pass
-        
+
         try:
             content = browser.find_elements_by_class_name('desc')
             content2 = [x.text for x in content]
             content_par =  '\n'.join(content2)
         except:
             pass
-        
+
         ### For each link, write to a .txt file
         fileName = "CorruptNews" +  str(counter) + ".txt"
         print(fileName)
@@ -76,7 +81,7 @@ for i in range(0,2): # number of search result pages 152
         textFile.write(content_par)
         textFile.close()
         browser.execute_script("window.history.go(-1)")
-        
+
     # Click next
     try:
         browser.find_element(By.XPATH, '//a[contains(text(), "Next")]').click()
